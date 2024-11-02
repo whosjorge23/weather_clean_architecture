8a1f37f4bbd3a3e69c46f4126e67c27bbf5fc8dd
Giorgio Giannotta <jorge_upa@msn.com>
2024-11-02 07:39:00 +0100

    add listviewBuilder daily weather card

---
+++ lib/features/weather/widgets/weather_day_card.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:weather_clean_architecture/core/shared/enums/weather_code_enum.dart';
+import 'package:weather_clean_architecture/features/weather/widgets/weather_code_text.dart';
+
+class WeatherDayCard extends StatelessWidget {
+  const WeatherDayCard({
+    Key? key,
+    required this.temperature,
+    required this.weatherCode,
+    required this.dateTime,
+  }) : super(key: key);
+
+  final String temperature;
+  final WeatherCodeEnum weatherCode;
+  final String dateTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: AspectRatio(
+        aspectRatio: 140 / 172,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Gap(8),
+                Text(
+                  temperature,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                const Spacer(),
+                WeatherCodeText(weatherCodeEnum: weatherCode),
+                const Spacer(),
+                Text(
+                  dateTime,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/weather/widgets/weather_page_content.dart
+++ lib/features/weather/widgets/weather_page_content.dart
@@ -5,6 +5,7 @@ import 'package:weather_clean_architecture/core/shared/context_extension.dart';
 import 'package:weather_clean_architecture/core/shared/enums/weather_code_enum.dart';
 import 'package:weather_clean_architecture/domain/entities/weather_response_entity.dart';
 import 'package:weather_clean_architecture/features/weather/widgets/weather_code_text.dart';
+import 'package:weather_clean_architecture/features/weather/widgets/weather_day_card.dart';
 import 'package:weather_clean_architecture/features/weather/widgets/weather_info.dart';
 
 class WeatherPageContent extends StatelessWidget {
@@ -21,82 +22,133 @@ class WeatherPageContent extends StatelessWidget {
       appBar: AppBar(
         title: Text(weatherEntity.timezone ?? ''),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Text(
-                DateFormat('EEEE, d MMMM').format(DateTime.now()),
-                style: const TextStyle(
-                  color: Color(0xff42c6ff),
-                ),
-              ),
-            ),
-            const Gap(16),
-            WeatherCodeText(
-                weatherCodeEnum: weatherEntity.current?.weatherCode),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '${weatherEntity.current?.apparentTemperature?.toInt()}',
-                    style: const TextStyle(fontSize: 150),
-                  ),
-                  Text(
-                    '${weatherEntity.currentUnits?.apparentTemperature}',
-                    style: const TextStyle(fontWeight: FontWeight.w500),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Container(
-                padding: const EdgeInsets.all(16),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
                 decoration: BoxDecoration(
                   color: Colors.black,
-                  borderRadius: BorderRadius.circular(8),
+                  borderRadius: BorderRadius.circular(16),
                 ),
+                child: Text(
+                  DateFormat('EEEE, d MMMM').format(DateTime.now()),
+                  style: const TextStyle(
+                    color: Color(0xff42c6ff),
+                  ),
+                ),
+              ),
+              const Gap(16),
+              WeatherCodeText(
+                  weatherCodeEnum: weatherEntity.current?.weatherCode),
+              Padding(
+                padding: const EdgeInsets.all(16),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    WeatherInfo(
-                      icon: Icons.wind_power,
-                      weatherInfo:
-                          '${weatherEntity.current?.windSpeed10m}${weatherEntity.currentUnits?.windSpeed10m}',
-                      label: context.l10n.wind,
+                    Text(
+                      '${weatherEntity.current?.apparentTemperature?.toInt()}',
+                      style: const TextStyle(fontSize: 150),
                     ),
-                    WeatherInfo(
-                      icon: Icons.water_drop,
-                      weatherInfo:
-                          '${weatherEntity.current?.relativeHumidity2m}${weatherEntity.currentUnits?.relativeHumidity2m}',
-                      label: context.l10n.humidity,
-                    ),
-                    WeatherInfo(
-                      icon: Icons.cloud,
-                      weatherInfo:
-                          '${weatherEntity.current?.cloudCover}${weatherEntity.currentUnits?.cloudCover}',
-                      label: context.l10n.clouds,
-                    ),
-                    WeatherInfo(
-                      icon: Icons.water_drop_outlined,
-                      weatherInfo:
-                          '${weatherEntity.current?.rain}${weatherEntity.currentUnits?.rain}',
-                      label: context.l10n.rain,
+                    Text(
+                      '${weatherEntity.currentUnits?.apparentTemperature}',
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Daily Summary',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
                     ),
                   ],
                 ),
               ),
-            )
-          ],
+              const Gap(16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      WeatherInfo(
+                        icon: Icons.wind_power,
+                        weatherInfo:
+                            '${weatherEntity.current?.windSpeed10m}${weatherEntity.currentUnits?.windSpeed10m}',
+                        label: context.l10n.wind,
+                      ),
+                      WeatherInfo(
+                        icon: Icons.water_drop,
+                        weatherInfo:
+                            '${weatherEntity.current?.relativeHumidity2m}${weatherEntity.currentUnits?.relativeHumidity2m}',
+                        label: context.l10n.humidity,
+                      ),
+                      WeatherInfo(
+                        icon: Icons.cloud,
+                        weatherInfo:
+                            '${weatherEntity.current?.cloudCover}${weatherEntity.currentUnits?.cloudCover}',
+                        label: context.l10n.clouds,
+                      ),
+                      WeatherInfo(
+                        icon: Icons.water_drop_outlined,
+                        weatherInfo:
+                            '${weatherEntity.current?.rain}${weatherEntity.currentUnits?.rain}',
+                        label: context.l10n.rain,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Gap(16),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Weekly Forecast',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+              const Gap(16),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: SizedBox(
+                  height: 172,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    physics: const ClampingScrollPhysics(),
+                    itemCount: weatherEntity.daily?.time?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      return WeatherDayCard(
+                        temperature:
+                            '${weatherEntity.daily!.apparentTemperatureMax![index].toInt()}°/${weatherEntity.daily!.apparentTemperatureMin![index].toInt()}°',
+                        weatherCode: weatherEntity.daily!.weatherCode![index],
+                        dateTime: DateFormat('d MMM').format(
+                          DateTime.parse(weatherEntity.daily!.time![index]),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
